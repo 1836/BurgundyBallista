fd93c13a254022ec2f054b9f2b19160b04c6a09c
Jake Reiner <jtreiner123@gmail.com>
2014-02-27 11:55:22 -0800

    Added IntakeSubsystem.java

---
+++ src/com/milkenknights/burgundyballista/IntakeSubsystem.java
@@ -0,0 +1,38 @@
+
+package com.milkenknights.burgundyballista;
+
+import edu.wpi.first.wpilibj.Talon;
+
+
+public class IntakeSubsystem {
+	Talon tIntake;
+	SolenoidPair sIntake;
+	JStick joystick;
+	
+	boolean solenoidOn;
+
+	
+	public IntakeSubsystem(RobotConfig config) {
+	tIntake = new Talon(config.getAsInt("tIntake"));
+	sIntake = new SolenoidPair(config.getAsInt("sIntakeA"),
+			config.getAsInt("sIntakeB"), true, true, false);
+	joystick = JStickMultiton.getJStick(2);
+}
+	
+	public void teleopPeriodic() {
+		if (joystick.isPressed(4)) {
+			tIntake.set(1);
+		}
+		if (joystick.isPressed(5)) {
+			tIntake.set(-1);
+		}
+		if (joystick.isReleased(4) || joystick.isReleased(5)) {
+			tIntake.set(0);
+		}
+		if (joystick.isPressed(2)) {
+			sIntake.set(!solenoidOn);
+			solenoidOn =! solenoidOn;
+		}
+	}
+	
+}
